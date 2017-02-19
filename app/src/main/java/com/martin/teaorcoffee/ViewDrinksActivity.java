04d112ccabb910ed9fe03e0c7f493bf4f88b326f
MartinT86 <martin.tierney86@hotmail.co.uk>
2017-02-19 20:31:56 +0000

    buttons aligned

--- app/src/main/java/com/martin/teaorcoffee/ViewDrinksActivity.java
+++ app/src/main/java/com/martin/teaorcoffee/ViewDrinksActivity.java
@@ -3,7 +3,9 @@ package com.martin.teaorcoffee;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
@@ -12,27 +14,48 @@ import java.util.ArrayList;
 
 public class ViewDrinksActivity extends AppCompatActivity {
 
+    private ArrayList<DrinkChoice> _drinks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_drinks);
 
         Intent intent = getIntent();
-        ArrayList<DrinkChoice> drinks = intent.getParcelableArrayListExtra(MainActivity.EXTRA_DRINKS);
+        _drinks = intent.getParcelableArrayListExtra(MainActivity.EXTRA_DRINKS);
+
+        DisplayDrinks();
+    }
+
+    public void removeDrink(View view){
+        int buttonId = view.getId();
+    }
+
+    private void DisplayDrinks() {
+
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_view_drinks_list);
 
-        for(DrinkChoice drinkChoice : drinks){
-            TextView textView = new TextView(this);
-            textView.setTextSize(40);
-
+        for(DrinkChoice drinkChoice : _drinks){
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
-            textView.setLayoutParams(params);
+            linearLayout.setLayoutParams(params);
 
+            TextView textView = new TextView(this);
+            textView.setTextSize(10);
             textView.setText(drinkChoice.Message());
+            LinearLayout.LayoutParams textViewParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
+            textView.setLayoutParams(textViewParams);
 
+            Button button = new Button(this);
+            LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            button.setLayoutParams(buttonParams);
+            button.setText("Remove");
 
-            layout.addView(textView);
+            linearLayout.addView(textView);
+            linearLayout.addView(button);
 
+            layout.addView(linearLayout);
         }
     }
 }
